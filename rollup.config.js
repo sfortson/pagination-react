d020c0ff2cb8802919638a105846e0d16dc5c6b0
Sam Fortson <sam.fortson@gmail.com>
2020-11-30 09:55:34 -0500

    Declare flow module exports to resolve bug

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import flow from 'rollup-plugin-flow';
 import babel from 'rollup-plugin-babel';
+import copy from 'rollup-plugin-cpy';
 import commonjs from '@rollup/plugin-commonjs';
 import flowEntry from 'rollup-plugin-flow-entry';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
@@ -10,7 +11,7 @@ export default {
   input: 'src/index.js',
   output: {
     file: pkg.main,
-    format: 'cjs',
+    format: 'es',
     exports: 'named',
     sourcemap: true,
     strict: false,
@@ -34,75 +35,11 @@ export default {
       comments: false,
     }),
     commonjs(),
+    // copy Flow definitions from source to destination directory
+    copy({
+      files: ['src/*.flow'],
+      dest: 'dist',
+    }),
   ],
   external: ['react', 'react-dom'],
 };
-// // plugins that we are going to use
-// import babel from 'rollup-plugin-babel';
-// import copy from 'rollup-plugin-cpy';
-// import flow from 'rollup-plugin-flow';
-// import commonjs from '@rollup/plugin-commonjs';
-// import { nodeResolve } from '@rollup/plugin-node-resolve';
-
-// // list of plugins used during building process
-// const plugins = (targets) => [
-//   nodeResolve(),
-//   // remove flow annotations from output
-//   flow(),
-//   // use Babel to transpile to ES5
-//   babel({
-//     // ignore node_modules/ in transpilation process
-//     exclude: 'node_modules/**',
-//     // ignore .babelrc (if defined) and use options defined here
-//     babelrc: false,
-//     // use recommended babel-preset-env without es modules enabled
-//     // and with possibility to set custom targets e.g. { node: '8' }
-//     presets: [['env', { modules: false, targets }]],
-//     // solve a problem with spread operator transpilation https://github.com/rollup/rollup/issues/281
-//     plugins: ['babel-plugin-transform-object-rest-spread'],
-//     // removes comments from output
-//     comments: false,
-//   }),
-//   commonjs(),
-//   // copy Flow definitions from source to destination directory
-//   copy({
-//     files: ['src/*.flow'],
-//     dest: 'lib',
-//   }),
-// ];
-
-// // packages that should be treated as external dependencies, not bundled
-// const external = []; // e.g. ['axios']
-
-// export default [
-//   //   {
-//   //     // source file / entrypoint
-//   //     input: 'src/index.js',
-//   //     // output configuration
-//   //     output: {
-//   //       // name visible for other scripts
-//   //       name: 'react-bulma-io-components',
-//   //       // output file location
-//   //       file: 'lib/index.esm.js',
-//   //       // format of generated JS file, also: esm, and others are available
-//   //       format: 'esm',
-//   //       // add sourcemaps
-//   //       sourcemap: true,
-//   //     },
-//   //     external,
-//   //     // build es modules for node 12
-//   //     plugins: plugins({ node: '12' }),
-//   //   },
-//   {
-//     input: 'src/index.js',
-//     output: {
-//       name: 'react-bulma-io-components',
-//       file: 'lib/index.js',
-//       format: 'cjs',
-//       sourcemap: true,
-//     },
-//     external,
-//     // build common JS for node 12
-//     plugins: plugins({ node: '12' }),
-//   },
-// ];
