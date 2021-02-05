0c03113c39f6fe49f23963976d76884e1bfc7d53
Sam Fortson <sam.fortson@gmail.com>
2021-02-05 08:51:40 -0500

    Finalize build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,22 @@
 import typescript from '@rollup/plugin-typescript';
+import copy from 'rollup-plugin-cpy';
 
 import pkg from './package.json';
 
 export default {
   input: 'src/index.ts',
-  output: {
-    dir: pkg.main,
-    format: 'cjs',
-  },
-  plugins: [typescript()],
+  output: [
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      dir: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+  ],
+  plugins: [typescript(), copy([{ files: 'src/**/*', dest: 'lib' }])],
   external: ['react', 'react/jsx-runtime'],
 };
